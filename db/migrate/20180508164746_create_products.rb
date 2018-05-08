8ad1c52bd741a26a3567a4636926acd6fd2c4e8e
monty <iacarril@miuandes.cl>
2018-05-08 13:53:36 -0300

    tables

--- db/migrate/20180508164746_create_products.rb
+++ db/migrate/20180508164746_create_products.rb
@@ -4,7 +4,7 @@ class CreateProducts < ActiveRecord::Migration[5.1]
       t.string :name
       t.integer :weight
       t.integer :price
-      t.integer :user_id
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
