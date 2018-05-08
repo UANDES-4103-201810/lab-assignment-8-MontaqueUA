7ad64985b3d41ecf7f854e09fb7c6373ebe426d9
monty <iacarril@miuandes.cl>
2018-05-08 15:02:35 -0300

    sidebar attempt

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
   resources :users do
     resources :products
   end
+  
   get '/admin/:id', to: 'users#admin_home'
   get '/admin/:id/users', to: 'users#admin_users'
   get '/admin/:id/products', to: 'users#admin_products'
   get '/admin/:id/users/:id/products', to: 'users#admin_user_product'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
